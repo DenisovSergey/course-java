9055012e5a5509444099076004a5344339522d56
DenisovSergey <dse-r@mail.ru>
2022-07-03 19:58:59 +0300

    the end added

--- src/ru/chebaksa/oop/Example1.java
+++ src/ru/chebaksa/oop/Example1.java
@@ -4,5 +4,6 @@ public class Example1 {
     public static void main(String[] args) {
         Cat dendy = new Cat("Dendy", "black", 1, true, 4);
         System.out.println(dendy.getHumanAge());
+        System.out.println("ENNDD");
     }
 }
